b96170fe8982b9e97cdbcda9f37910d95d023867
Matia-k <matia40k@gmail.com>
2023-08-02 02:04:47 -0700

    updated header file

--- binary_trees.h
+++ binary_trees.h
@@ -13,20 +13,16 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
+}binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 void binary_tree_print(const binary_tree_t *);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-static size_t _height(const binary_tree_t *tree);
 
 #endif
