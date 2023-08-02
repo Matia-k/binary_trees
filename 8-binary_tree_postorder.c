9b7ba7a80728c2fcdb88f3757a18b5ee94c2c3b4
Matia-k <matia40k@gmail.com>
2023-08-02 10:53:28 -0700

    corrected order of traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_postorder - traverses a binary tree using in-order traversal.
+* binary_tree_postorder - traverses a binary tree using post-order traversal.
 * @tree: the tree to traverse.
 * @func: a pointer to a function to call for each node.
 * The value in the node must be passed as a parameter to this function.
@@ -11,7 +11,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree && func)
 	{
 		binary_tree_postorder(tree->left, func);
-		func(tree->n);
 		binary_tree_postorder(tree->right, func);
+		func(tree->n);
 	}
 }
