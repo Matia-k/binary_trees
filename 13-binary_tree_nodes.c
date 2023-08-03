70e573f91070845fdbb4cb868b5ab74244b0b165
Matia-k <matia40k@gmail.com>
2023-08-03 11:09:25 -0700

    tasks 12 and 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,5 +7,14 @@
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+	size_t count = 0;
+
+	if (!tree)
+		return (0);
+	
+	count = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+	
+	if (tree->left || tree->right)
+		count++;
+	return (count);
 }
